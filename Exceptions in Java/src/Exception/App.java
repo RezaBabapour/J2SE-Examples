91d50267bd90c1fcfde217f428e9e9d75fdd80a9
Reza Babapour <reza.babapour91@gmail.com>
2019-04-27 14:48:32 +0430

    Exeption handling

---
+++ Exceptions in Java/src/Exception/App.java
@@ -0,0 +1,10 @@
+package Exception;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("before code with exception.");
+        System.out.println(1/0);
+        System.out.println("after exception.");
+    }
+}
