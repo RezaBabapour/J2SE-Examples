0028814291cdb08297870bc57915df6d1bb2ae7f
Reza Babapour <reza.babapour91@gmail.com>
2019-06-05 11:09:30 +0430

    example added

---
+++ 19 JDBC/src/DB/main.java
@@ -0,0 +1,19 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class main {
+    public static void main(String[] args) throws Exception {
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:42333/?user=root&password=root");
+        Statement statement = con.createStatement();
+        statement.executeUpdate("INSERT INTO `beYad`.`person` (`userName`) VALUES ('ali')");
+        statement.executeUpdate("INSERT INTO `beYad`.`person` (`userName`) VALUES ('ali')");
+        int i1 = statement.executeUpdate("DELETE FROM beYad.person WHERE userName='ali'");
+        System.out.println(i1);
+        statement.close();
+        con.close();
+
+    }
+}
